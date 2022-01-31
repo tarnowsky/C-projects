cbbb6666115265f399e19e31060c15b17169372c
Michal Tarnowski <michal.tarnowski03@gmail.com>
2022-01-31 01:05:19 +0100

    init

---
+++ InvertedSquareRoot_Quake/inverseSquareRoot.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+float Qrsqrt( float number )
+{
+    long i;
+    float x2, y;
+    const float threehalfs = 1.5F;
+
+    x2 = number * 0.5F;
+    y = number;
+    i = * ( long * ) &y;
+    i = 0x5f3759df - ( i >> 1 );
+    y = * ( float * ) &i;
+    y = y * ( threehalfs - (x2 * y * y) );
+    y = y * ( threehalfs - (x2 * y * y) );
+
+    return y;
+
+}
+int main() {
+    float n = 100;
+
+    cout << Qrsqrt(n);
+    
+}
