e5662ce0f285dba4587209fe8c60829cfd4f99e8
Michal Tarnowski <michal.tarnowski03@gmail.com>
2022-07-03 12:37:59 +0200

    reverse func

--- book excercises/main.c
+++ book excercises/main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
-    int i, k;
-    char c[36], a;
+    int j, i, k;
+    char c[30] = {'1', '2', '3', '4'};
 
-    for (k=0;(c[k] = getchar()) != '\n'; k++);
-    for (i=0; i < k; i++)
-        printf("%c", (c[i] >= 'A' && c[i] <= 'Z') ? c[i] - 'A' + 'a' : c[i]);
+    for (i=0, j = strlen(c)-1; i<j; i++, j--)
+        k = c[i], c[i] = c[j], c[j] = k;
+    
+    for (i=0; c[i]!='\0'; i++)
+        printf("%c", c[i]);
 
 
 
